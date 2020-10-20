f37b3ca721da59355a979b7313912e71bbcc447d
gustirandabone <gustiranda147@gmail.com>
2020-10-20 19:24:13 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus minus tenetur exercitationem quam saepe, labore eaque, dicta molestias, totam ut dignissimos qui porro sed? Accusantium, repellat neque. Repellat, beatae fugit.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
